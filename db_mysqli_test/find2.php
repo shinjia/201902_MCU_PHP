bd61bb31e2dbaeeb75dbb5da995658a17eedaef3
Shinjia <shinjia168@gmail.com>
2019-04-23 22:04:39 +0800

    Class09

---
+++ db_mysqli_test/find2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>基本資料庫系統</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<h2>查詢資料</h2>
+<form action="find2_x.php" method="post">
+<p>
+  姓名：<input type="text" name="key"><br />
+</p>
+<p><input type="submit" value="查詢"></p>
+</form>
+</body>
+</html>
